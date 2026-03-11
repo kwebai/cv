4de7027a164bacfa43f61d6f1c383e06d7966e33
kwebai <162511264+kwebai@users.noreply.github.com>
2026-03-11 12:43:59 +0100

    Initial commit

--- index.php
+++ index.php
@@ -1,9 +1,3 @@
 <?php
 
-use App\Kernel;
-
-require dirname(__DIR__).'/data/project/vendor/autoload_runtime.php';
-
-return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
-};
+require dirname(__DIR__).'/html/public/index.php';
